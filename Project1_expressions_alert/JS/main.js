b62c19be2a116fde371574aacdfa6ae13f166a3d
TRK360 <117139528+TRK360@users.noreply.github.com>
2022-12-07 13:34:49 -0500

    ACTUAL Submittal
    For assignment submittal.

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("welcome to my JavaScript submission.")
+
+var A="This is the beginning of the string",B=" and this is the end of the string\'s content."
+
+
+document.write(A+B);  //this is concantenating the two variables
